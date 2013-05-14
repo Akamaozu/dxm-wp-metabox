0517be9711555a890fb47fef9d172e3fb754ea1a
Uzo Olisemeka <uzo@designbymobius.ca>
2013-05-14 11:51:36 -0400

    Revamped Class
    Also added README.md

---
+++ README.md
@@ -0,0 +1,331 @@
+## Metabox Maker ##
+
+Making it **ridiculously easy** for you to create metaboxes for Wordpress Edit Screens.
+
+- Create a single box or multiple boxes at once with a few lines of code.
+
+- Specify the conditions to be met before creating individual boxes, set conditions for a group of boxes or mix-and-match as you please.
+
+- Provides easy-to-use hooks to save and autosave events. What you choose to do then (and how you want to filter it) is up to you. Metabox Maker supplies the hooks for you to use as you please.
+
+- Don't need the default content editor? Remove it easily. Remove it only under specific conditions? Not a problem!
+
+
+----------
+
+
+### HOW IT WORKS ###
+
+
+----------
+
+
+1. Create a new Metabox Maker instance to work with.
+
+    `$mm = new MetaboxMaker();`
+
+2. Create the metabox you need.
+
+		$mm->create( 
+			array(
+				"id" => "mm_testbox", 
+				"post_type" => "post", 
+				"markup" => "mm_testbox_markup" 
+			) 
+		);
+
+3. Wrap it in a function and set it to run on `admin_init` hook.
+
+
+		function create_test_metabox(){
+			
+			// include Metabox Maker class source
+			require_once("/path/to/metabox.php");
+			
+			// create a new instance of Metabox Maker
+			$mm-> new MetaboxMaker();
+			
+			// specify required metabox parameters
+			$mm->create( 
+				array( 
+					"id" => "mm_testbox", 
+					"post_type" => "post", 
+					"markup" => "mm_testbox_markup" 
+				) 
+			);
+		}
+
+   		function mm_testbox_markup(){
+			
+			// html markup for the metabox			
+			echo "<h1>Hello World!</h1>";
+		}
+
+		add_action('admin_init', 'create_test_metabox');
+
+
+4. Rinse and repeat as needed.
+
+
+----------
+
+### DOING MORE THAN THE BASICS ###
+
+----------
+
+- Create Metabox for multiple Post Types
+
+		$mm->create( 
+			array(
+				"id" => "mm_testbox", 
+				"post_type" => array("post", "page", "custom_post_type"), 
+				"markup" => "mm_testbox_markup" 
+			) 
+		);
+
+- Create Multiple Metaboxes at once
+
+	    $mm->create(
+			array(
+				
+				array(					
+					"id" => "first_box",
+					"post_type => "post",
+					"markup" => "first_box_markup" 
+				),
+
+				array(										
+					"id" => "second_box",
+					"post_type => array("post", "page"),
+					"markup" => "second_box_markup" 
+				)
+			)
+		);
+
+- Create a Metabox only when specified conditions are true
+
+		// create metabox only if the page slug is "about-us"
+
+		$mm->create(
+ 
+			array(
+				"id" => "about_us_metabox", 
+				"post_type" => "page", 
+				"markup" => "about_us_metabox_markup"
+				"conditions" => function(){
+					
+					global $post;
+										
+					if ($post->post_name == 'about-us'){			
+						return true;
+					}
+					
+					return false;
+				}
+			) 
+		);
+
+- Set Conditions for all Metaboxes created by an Instance of Metabox Maker
+
+		// $mm will create Metaboxes only on draft post edit screens
+		
+		$mm = new MetaboxMaker( 
+			
+			array(
+				
+				"conditions" => function(){
+		
+					global $post;
+					
+					if( $post->post_type == "post" && ($post->post_status == "draft" || $post->post_status == "auto-draft") ) {
+						return true;
+					}
+		
+					return false;
+				}
+			)
+		);
+ 
+
+
+----------
+
+### PASSING PARAMETERS
+
+----------
+
+
+When Instantiating Metabox Maker 
+-
+	
+	// NOTE: $params is optional
+	// You can instantiate Metabox Maker without it
+
+	$mm = new MetaboxMaker( $params );
+
+### About $params ###
+
+- is an array
+	
+### What's Available ###
+
+1. **$params[** 'conditions' **]**
+
+	*Function that returns bool true/false. If false, $mm will not create any metaboxes, regardless of individual metabox creation conditions.*
+
+
+When Creating a Metabox
+-
+	
+
+	
+	// NOTE: $args is required
+	$mm->create( $args );	
+	
+
+### About $args ###
+- is required
+- is an array
+- can be an array of a single metabox's properties
+	
+		// single metabox
+		$args = array(
+			
+			"id" => "about_us_metabox", 
+			"post_type" => "page", 
+			"markup" => "about_us_metabox_markup"
+		)
+
+- can be an array containing multiple "single metabox properties"
+
+		// multiple metaboxes
+		$args = array (
+			
+			array(
+				
+				"id" => "about_us_metabox", 
+				"post_type" => "page", 
+				"markup" => "about_us_metabox_markup"
+			),
+		
+			array(
+			
+				"id" => "contact_us_metabox", 
+				"post_type" => "page", 
+				"markup" => "contact_us_metabox_markup"
+			)
+		)
+
+### What's Available (for Single Metabox properties)###
+1. **$args[** 'id' **]** *(required)*
+	
+	*String containing a unique ID for your metabox. Will be used in the backend and also accessible via CSS.*
+
+2. **$args[** 'markup' **]** *(required)*
+
+	*String name of the function responsible for creating the HTML inside a metabox.*
+
+3. **$args[** 'post_type' **]** *(required)*
+
+	*String or Array containing post types to create the metabox on.* 
+
+	*// single post type*
+	
+		$args[ 'post_type' ] => "post"
+
+	*// multiple post types*
+		
+		$args[ 'post_type' ] => array( "post",  "page" )
+
+4. **$args[** 'title' **]**
+	
+	*String containing the title used in the metabox header and screen options menu.*
+
+	*Default is blank.*
+
+5. **$args[** 'context' **]** 
+
+	*String indicating the section of the page to place the metabox*
+
+	*Options: Normal, Advanced, Side*
+ 
+	*Default: Advanced*
+	
+6. **$args[** 'Priority' **]** 
+
+	*String indicating the order in which metaboxes are placed in their section*
+
+	*Options: High, Core, Default, Low*
+
+	*Default: Default*
+
+7. **$args[** 'conditions' **]**
+
+	*Function for determining if metabox should be rendered or not*
+
+	*Must return boolean true if condition met or false if not met*
+
+8. **$args[** 'pass_params' **]**
+
+	*Variable to pass to metabox on creation, during save or autosave*
+
+	*Variable can be any type of your choice*
+
+9. **$args[** 'on_autosave' **]**
+
+	*Function to execute when autosave is triggered*
+
+	*Will not trigger if it's a user or function-triggered save. Only works on autosave*
+
+10. **$args[** 'on_save' **]**
+
+	*Function to execute when save is triggered (by user or by programmatically)*
+
+	*Will not trigger if Wordpress is doing an autosave*
+
+
+Added Functionality
+-
+
+- Remove Default Editor 
+	
+	`$mm->remove_editor( $conditions );`
+
+	*Need to get rid of the default WYSIWYG Editor? Use the* `remove_editor` *function*
+
+	*Set conditions to determine  when the function will trigger or not*
+
+	`remove_editor` *is subject to Metabox Maker's instantiation conditions*
+
+	### About $conditions ###
+
+	1. is a function
+	2. returns boolean true if conditions are met; otherwise returns false
+
+			$mm->remove_editor(
+				
+				function(){
+					
+					global $post;
+		
+					if ($post->ID > 9000){						
+						return true;
+					}
+		
+					return false;
+				}
+			);
+
+
+
+
+
+----------
+
+#That's all, Folks!#
+
+----------
+
+Got tips to improve this? **Awesome**! 
+
+Send an email to  `uzo@designbymobius.ca` or we can do the Github thing. Whichever is more convenient.
--- metabox.php
+++ metabox.php
@@ -1,81 +1,229 @@
 <?php
 
-class metabox_handler {
+class MetaboxMaker {
 
-	function create($boxesToMake) {
-	
-	/*  
-	=================================================
-	HOW 'metabox_handler->create($boxesToMake)' WORKS
-	=================================================
-	
-	$boxesToMake = array containing arrays of metaboxes to build and their properties
-	
-		ex. $boxesToMake = array (
-			array (
-				'id' => 'example1',
-				'title' => 'Example of a Metabox 1',
-				'callback' => 'ui_for_example1'
-			),
-			
-			array (
-				'id' => 'example2',
-				'title' => 'Example of a Metabox 2',
-				'callback' => 'ui_for_example2'
-			)
-		);
-		
-		foreach ($boxesToMake as $metabox) 
-		$metabox[args] = array( id, title, callback, posttype, context, priority, callback args);
-			
-			id = HTML id [string]
-			title = Metabox Title [string]
-			callback = Function responsible for markup inside the metabox [string]
-			posttype = Edit screen to display the metabox in [string, array]-> {post, page, link, custom_post_type}
-			context = Position in the Post Page [string]-> {normal, advanced, side}
-			priority = Vertical Position in assigned context [string]-> {high, core, default, low}
-			callbackargs = parameters to pass to callback function [string, array]
-			
+	var $boxes_to_make = array(),
+		$init_params;
 
-	*/
-		
-		foreach ($boxesToMake as $metabox_name => $metabox) {
-		
-			// Metabox shows up on multiple types of page edit screens
-			if (is_array($metabox['posttypes'])) {
-				
-				foreach ( $metabox['posttypes'] as $posttype) { 
-					add_meta_box( 
-					$metabox['id'], 
-					$metabox['title'], 
-					$metabox['callback'], 
-					$posttype, 
-					$metabox['context'], 
-					$metabox['priority'], 
-					$metabox['callbackargs']
-					);
-				}
-			
-			} 
-			
-			// Metabox shows up on a single type of page edit screen
-			else {
-				add_meta_box( 
-				$metabox['id'], 
-				$metabox['title'], 
-				$metabox['callback'], 
-				$metabox['posttypes'], 
-				$metabox['context'], 
-				$metabox['priority'],
-				$metabox['callbackargs']
-				);
-			
-			}
-			
+
+
+	private function is_metabox_data($data){
+
+		if(!is_array($data) || empty($data) || !$data['id'] || !$data['markup'] || !$data['post_type']){
+
+			return false;
 		}
 
+		return true;
 	}
-	
-}
 
-?>
+	private function detect_input_type($input){
+
+		// default return value
+		$input_type = 'invalid';
+
+		$is_metabox = $this->is_metabox_data($input);
+
+		// filter out improper or empty $input
+		if ( $is_metabox  == false ) {
+
+			return $input_type;
+		}
+
+		// confirm proper $input
+		if ( $is_metabox == true ) {
+
+			$input_type = 'single';
+			return $input_type;
+		}
+
+		// check if $input is an array of metabox data 
+		foreach ($input as $nested_input){
+
+			if ( $this->is_metabox_data($nested_input) == true ) {
+
+				$input_type = 'multiple';
+				return $input_type;
+			}
+		}
+
+		// not single, not multiple, not empty, not usable -- invalid
+		return $input_type;
+	}
+
+	private function set_defaults($metabox_data) {
+
+		// conditionals with default values if unset 
+		$metabox_data['title'] = ( isset ( $metabox_data['title'] ) ? $metabox_data['title'] : "" );
+		$metabox_data['context'] = ( isset( $metabox_data['context'] ) ? $metabox_data['context'] : "advanced" );
+		$metabox_data['priority'] = ( isset( $metabox_data['priority'] ) ? $metabox_data['priority'] : "default" );
+		$metabox_data['conditions'] = ( isset( $metabox_data['conditions'] ) ? $metabox_data['conditions'] : NULL );
+		$metabox_data['pass_params'] = ( isset( $metabox_data['pass_params'] ) ? $metabox_data['pass_params'] : NULL );
+		$metabox_data['on_autosave'] = ( isset( $metabox_data['on_autosave'] ) ? $metabox_data['on_autosave'] : NULL );
+		$metabox_data['on_save'] = ( isset( $metabox_data['on_save'] ) ? $metabox_data['on_save'] : NULL );
+
+		return $metabox_data;
+	}
+
+	function create_save_hook($metabox_data){
+
+		add_action( 'save_post', function($post_id, $metabox_data){
+
+			// filter out unpermitted users
+			if ( 'page' == $_POST['post_type'] ||  'post' == $_POST['post_type']) {
+		        
+		        if ( !current_user_can( 'edit_page', $post_id ) || !current_user_can( 'edit_post', $post_id )){
+		          
+		          return $post_id;
+		        }
+			}
+
+			// do autosave if set
+			if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE && $metabox_data['on_autosave'] != NULL ){
+
+				$metabox_data['on_autosave']( $metabox_data['pass_params'] );
+			}
+
+			// do regular save if set
+			else if ($metabox_data['on_save']) {
+
+				$metabox_data['on_save']( $metabox_data['pass_params'] );
+			}
+		});
+	}
+
+	function create_for_post_type($metabox_data, $post_type){
+
+		add_meta_box(
+
+			$metabox_data['id'],
+			$metabox_data['title'],
+			$metabox_data['markup'],
+			$post_type,
+			$metabox_data['context'],
+			$metabox_data['priority'],
+			$metabox_data['pass_params']
+		);
+	}
+
+	function meets_conditions( $metabox_data = array() ){
+
+		if( 
+			( isset($metabox_data['conditions']) && $metabox_data['conditions']() == false ) ||
+			( isset($this->init_params['conditions']) && $this->init_params['conditions']() == false ) 
+		) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	function setup_construction(){
+
+		// if action hook hasn't been declared, do it
+		if ( !has_action( 'add_meta_boxes', array($this, 'init_construction')) ) {
+
+			add_action( 'add_meta_boxes', array($this, 'init_construction') );
+		}
+	}
+
+	function queue_for_construction($metabox_data){
+
+		$this->boxes_to_make[] = $metabox_data;
+	}
+
+	function init_construction(){
+
+		foreach ($this->boxes_to_make as $metabox_data){
+
+			$this->create_box($metabox_data);
+		}
+	}
+
+	function create_box($metabox_data){
+
+		// set defaults for unset variables
+		$metabox_data = $this->set_defaults($metabox_data);
+
+		// create for one post type
+		if ( is_string($metabox_data['post_type']) && $this->meets_conditions($metabox_data) == true ) {
+
+			$this->create_for_post_type($metabox_data, $metabox_data['post_type']);
+		}
+
+		// create for multiple post types
+		else if ( is_array($metabox_data['post_type']) ){
+
+			foreach ($metabox_data['post_type'] as $post_type) {
+				
+				if ( $this->meets_conditions($metabox_data) == true ){
+					
+					$this->create_for_post_type($metabox_data, $post_type);
+				}
+			}
+		}
+
+		// if needed, create save hooks
+		if ( $metabox_data['on_save'] != NULL || $metabox_data['on_autosave'] != NULL ){
+
+			$this->create_save_hook($metabox_data);
+		}
+	}
+
+	function create($data) {
+
+		switch ( $this->detect_input_type($data) ) {
+
+			case 'single':
+
+				$metabox_data = $data;
+				$this->queue_for_construction( $metabox_data );
+
+				$this->setup_construction();
+			break;
+
+			case 'multiple':
+
+				foreach ($data as $metabox_data) {
+
+					$this->queue_for_construction( $metabox_data );
+				}
+
+				$this->setup_construction();
+			break;
+
+			case 'invalid':
+			default:
+
+				// no good data to work with ... bail!
+			break;
+		}
+	}
+
+	function remove_content_editor() {
+		
+		if ( $this->meets_conditions() == true ) {
+		
+			global $_wp_post_type_features;
+
+			foreach ($_wp_post_type_features as $type => &$features) {
+				if ( isset($features['editor']) && $features['editor'] ) {
+					
+					unset( $features['editor'] );
+				}
+			}
+		}
+	}
+
+	function remove_editor(){
+	
+		add_action( 'add_meta_boxes', array( $this, 'remove_content_editor' ), 0 );
+	}
+
+	function __construct($init_params){
+
+		$this->init_params = $init_params;
+	}
+} 
+?> 
